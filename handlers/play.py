412090ae2a9ee97006c4f80b3008f63692a75bfa
xdutss <90018967+xdutss@users.noreply.github.com>
2021-10-07 15:48:21 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -726,7 +726,7 @@ async def lol_cb(b, cb):
     if cb.from_user.id != useer_id:
         await cb.answer("💡 sorry, this is not for you !", show_alert=True)
         return
-    # await cb.message.edit("🔁 **processing...**")
+    await cb.message.edit("🔁 **processing...**")
     x = int(x)
     try:
         cb.message.reply_to_message.from_user.first_name
